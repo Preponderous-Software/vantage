384a4ac77362851f9e8d5f337c67e7e704f28d7e
dmccoystephenson <dmccoystephenson@gmail.com>
2024-02-18 14:47:00 -0700

    Added Dockerfile & compose.yml for backend

--- backend/src/main/kotlin/uk/co/renbinden/vantage/config/DatabaseConfig.kt
+++ backend/src/main/kotlin/uk/co/renbinden/vantage/config/DatabaseConfig.kt
@@ -1,8 +1,8 @@
 package uk.co.renbinden.vantage.config
 
 data class DatabaseConfig(
-    val url: String = "jdbc:postgresql://localhost/vantage",
-    val username: String? = null,
-    val password: String? = null,
+    val url: String = "jdbc:postgresql://backend-vantagedb-1/vantage",
+    val username: String? = "vantage",
+    val password: String? = "vantage",
     val dialect: String = "POSTGRES"
 )
