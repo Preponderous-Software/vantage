cc99a33a621388cfdc8d42756020b86971717826
dmccoystephenson <dmccoystephenson@gmail.com>
2024-02-20 22:36:11 -0700

    Moved `compose.yml` to root directory and reworked backend Dockerfile a bit

--- backend/src/main/kotlin/uk/co/renbinden/vantage/config/DatabaseConfig.kt
+++ backend/src/main/kotlin/uk/co/renbinden/vantage/config/DatabaseConfig.kt
@@ -1,7 +1,7 @@
 package uk.co.renbinden.vantage.config
 
 data class DatabaseConfig(
-    val url: String = "jdbc:postgresql://backend-vantagedb-1/vantage",
+    val url: String = "jdbc:postgresql://vantage_vantagedb_1/vantage",
     val username: String? = "vantage",
     val password: String? = "vantage",
     val dialect: String = "POSTGRES"
